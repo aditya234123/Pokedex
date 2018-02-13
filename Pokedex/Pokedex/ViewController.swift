2332435c0416a9b1cd35a21deffb77776cc04277
Aditya Yadav and Ethan Wong <pair+aditya234123+ethanwong987@pivotallabs.com>
2018-02-13 13:25:14 -0800

    issues fixed

--- Pokedex/Pokedex/ViewController.swift
+++ Pokedex/Pokedex/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let searchViewController = SearchViewController()
+        let searchNavController = UINavigationController(rootViewController: searchViewController)
+        searchNavController.tabBarItem.title = "Search"
+        searchNavController.tabBarItem.image = UIImage(named: "search")
+        
+        let favoritesViewController = FavoritesViewController()
+        let favNavController = UINavigationController(rootViewController: favoritesViewController)
+        favNavController.tabBarItem.title = "Favorites"
+        favNavController.tabBarItem.image = UIImage(named: "star")
+        
+        viewControllers = [searchNavController, favNavController]
+        
     }
 
     override func didReceiveMemoryWarning() {
