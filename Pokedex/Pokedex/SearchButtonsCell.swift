c8beb201cd8d388f77c5325b9779b5e66cb04bff
Aditya Yadav and Ethan Wong <pair+aditya234123+ethanwong987@pivotallabs.com>
2018-02-13 15:38:35 -0800

    first page lookin nice

--- Pokedex/Pokedex/SearchButtonsCell.swift
+++ Pokedex/Pokedex/SearchButtonsCell.swift
@@ -12,11 +12,13 @@ class SearchButtonsCell: UICollectionViewCell {
  
     var button: UIButton!
     
+    let numToType = [1 : "BUG", 2 : "DARK", 3 : "DRAGON", 4 : "ELECTRIC", 5 : "FAIRY", 6 : "FIGHTING", 7 : "FIRE", 8 : "FLYING", 9 : "GHOST", 10 : "GRASS", 11 : "GROUND", 12 : "ICE", 13 : "NORMAL", 14 : "POISON", 15 : "PSYCHIC", 16 : "ROCK", 17 : "STEEL", 18 : "WATER"]
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
         button = UIButton(frame: contentView.frame)
-        button.layer.cornerRadius = 10
+        button.layer.cornerRadius = 5
         
         contentView.addSubview(button)
         
@@ -26,7 +28,16 @@ class SearchButtonsCell: UICollectionViewCell {
         let strName = "\(num)"
         let image = UIImage(named: strName)
         self.button.setImage(image, for: .normal)
+        button.imageEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 30, right: 20)
+        button.setTitle(numToType[num], for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Georgia", size: 13)
+        button.titleEdgeInsets = UIEdgeInsets(top: contentView.frame.height - 27, left: -(button.imageView?.image?.size.width)!, bottom: 10, right: 0)
+        let bgColor = UIColor(red: 24/255, green: 45/255, blue: 64/255, alpha: 1.0)
+        button.backgroundColor = bgColor
     }
+    
+    
     
     
     
--- Pokedex/Pokedex/ViewControllers/ViewController.swift
+++ Pokedex/Pokedex/ViewControllers/ViewController.swift
@@ -16,13 +16,16 @@ class ViewController: UITabBarController {
         
         let searchViewController = SearchViewController()
         let searchNavController = UINavigationController(rootViewController: searchViewController)
-        searchNavController.tabBarItem.title = "Search"
-        searchNavController.tabBarItem.image = UIImage(named: "search")
+        searchNavController.tabBarItem.title = "Filter"
+        searchNavController.tabBarItem.image = UIImage(named: "filter")
         
         let favoritesViewController = FavoritesViewController()
         let favNavController = UINavigationController(rootViewController: favoritesViewController)
         favNavController.tabBarItem.title = "Favorites"
         favNavController.tabBarItem.image = UIImage(named: "star")
+        
+        let bgColor = UIColor(red: 18/255, green: 33/255, blue: 49/255, alpha: 1.0)
+        self.tabBar.barTintColor = bgColor
         
         viewControllers = [searchNavController, favNavController]
         
