1a0f8ddc5b36d399a9872c2f4a71e610a2592bbd
Aditya Yadav and Ethan Wong <pair+aditya234123+ethanwong987@pivotallabs.com>
2018-02-15 21:59:02 -0800

    YESS ITS WHITE

--- Pokedex/Pokedex/ViewControllers/ViewController.swift
+++ Pokedex/Pokedex/ViewControllers/ViewController.swift
@@ -71,7 +71,7 @@ extension ViewController: SearchControllerDelegate {
     func setNavBarTitle(name: String) {
         self.title = name
         self.navigationController?.navigationBar.tintColor = .white
-       
+
     }
 
 }
